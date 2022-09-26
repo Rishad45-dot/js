e11c2d413f981f29aca8836496af6ef5db152093
Ashikur R <saieidashik@outlook.com>
2022-09-26 16:09:17 +0600

    Meta Course on Coursera week1

--- week1.js
+++ week1.js
@@ -57,3 +57,14 @@ console.log( 12 + ' months') //number 12 automatically gets converted to string 
  */
 console.log(5>4>3) //returns false, because the 5 > 4 is evaluated first (to `true`), then true > 3 is evaluated to `false`, because the `true` value is coerced to `1`
 
+/**
+ * Modulas operator %
+ */
+var num1 = 2
+var num2 = 5
+var test1 = num1 % 2
+var test2 = num2 % 2
+var result1 = test1 == 0
+var result2 = test2 == 0
+console.log('Is',num1,'an even number?',result1)
+console.log('Is',num2,'an even number?',result2)
